3cb9d44392486606b11e5594d1ad73460a4f2a13
emmanuel.lion <drakenunity@gmail.com>
2026-09-16 22:47:44 +0200

    4-gramme

--- bigramme/main.py
+++ bigramme/main.py
@@ -1,8 +1,10 @@
 import math
 import torch
 
-# block_size = 8
-# batch_size = 32
+block_size = 3
+batch_size = 64
+n_embd = 10
+n_hidden = 200
 
 with open("text.txt", "r", encoding="utf-8") as f:
     text = f.read()
@@ -18,61 +20,71 @@ encode = lambda s: [stoi(c) for c in s]
 decode = lambda i: ''.join([itos(j) for j in i])
 
 data = torch.tensor(encode(text), dtype=torch.long)
-# stop = math.floor(len(text) * 0.9)
-# train = torch.tensor(encode(text[:stop]), dtype=torch.long)
-# val = torch.tensor(encode(text[stop:]), dtype=torch.long)
+stop = math.floor(len(text) * 0.9)
+train = torch.tensor(encode(text[:stop]), dtype=torch.long)
+val = torch.tensor(encode(text[stop:]), dtype=torch.long)
 
-# def get_batch(split):
-#     if split == "train":
-#         values = train
-#     else:
-#         values = val
-#     x_batchs = []
-#     y_batchs = []
-#     starts = torch.randint(high=len(values) - block_size, size=(batch_size,))
-#     for start in starts:
-#         picked = values[start:start + block_size]
-#         x_batchs.append(picked)
-#         y_batchs.append(values[start+1:start + block_size + 1])
-#     x = torch.stack(x_batchs)
-#     y = torch.stack(y_batchs)
-#     return x, y
-#
-#
-# class Bigram(torch.nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.embedding = torch.nn.Embedding(vocab_size, vocab_size)
-#
-#     def forward(self, v):
-#         return self.embedding(v)
+def get_batch(split):
+    if split == "train":
+        values = train
+    else:
+        values = val
+    x_batchs = []
+    y_batchs = []
+    starts = torch.randint(high=len(values) - block_size, size=(batch_size,))
+    for start in starts:
+        x_batchs.append(values[start:start + block_size])
+        y_batchs.append(values[start + block_size])
+    x = torch.stack(x_batchs)
+    y = torch.stack(y_batchs)
+    return x, y
 
 
-count = torch.load('count.pt', weights_only=True)
+class Bigram(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.emb = torch.nn.Embedding(vocab_size, n_embd)
+        self.hidden = torch.nn.Linear(block_size * n_embd, n_hidden)
+        self.out = torch.nn.Linear(n_hidden, vocab_size)
 
-# count = torch.zeros((vocab_size, vocab_size))
-# for i, c in enumerate(text):
-#     if i+1 < len(text):
-#         c1 = c
-#         c2 = text[i + 1]
-#         count[stoi(c1), stoi(c2)] += 1
-# torch.save(count, 'count.pt')
+    def forward(self, v):
+        embedded = self.emb(v).view(-1, block_size * n_embd)
+        hidden = self.hidden(embedded)
+        relu = torch.relu(hidden)
+        return self.out(relu)
 
-# soft = torch.softmax(count, dim=-1)
-# count += 1
-prob = count / count.sum(dim=1, keepdim=True)
-top = torch.topk(prob[stoi("q")], k=5, dim=-1)
-idx = top.indices
-vals = top.values
-# for i in range(0, len(top.indices)):
-#     print(f"'{itos(idx[i].item())}': {vals[i].item()}")
 
-ids = [stoi("L")]
-for i in range(0, 300):
-    last_prob = prob[ids[-1]]
-    new_id = torch.multinomial(last_prob, 1)
-    ids.append(new_id.item())
-print(''.join(decode(ids)))
-x = data[:-1]
-y = data[1:]
-print(-torch.log(prob[x, y]).mean().item())
+def generate(_model):
+    with torch.no_grad():
+        ids = data[:block_size].tolist()
+        for i in range(0, 300):
+            batch = torch.tensor([ids[-block_size:]])
+            logits = _model(batch)
+            prob = torch.softmax(logits, dim=-1)
+            new_id = torch.multinomial(prob, 1)
+            ids.append(new_id.item())
+        print(decode(ids))
+
+
+def get_loss(_model: Bigram, split: str) -> torch.Tensor:
+    entrees, cibles = get_batch(split)
+    logits = _model.forward(entrees)
+    return torch.nn.functional.cross_entropy(logits, cibles)
+
+model = Bigram()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+generate(model)
+for i in range(0, 10000):
+    loss = get_loss(model, "train")
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    # if i % 1000 == 0:
+    #     with torch.no_grad():
+    #         loss_train = []
+    #         loss_val = []
+    #         for _ in range(0, 50):
+    #             loss_train.append(get_loss(model, "train"))
+    #             loss_val.append(get_loss(model, "val"))
+    #         print(f"Round {i}:\nTrain: {torch.stack(loss_train).mean()}\nVal: {torch.stack(loss_val).mean()}")
+generate(model)
